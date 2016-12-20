6e3f5fe58fc169d63ba1e827257c19ca1502140d
hewater <270232645@qq.com>
2016-12-20 18:11:28 +0800

    nothing to say

--- yydp/src/test/java/yydp/test/load/FilmLoaderTest.java
+++ yydp/src/test/java/yydp/test/load/FilmLoaderTest.java
@@ -1,5 +1,9 @@
 package yydp.test.load;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,7 +15,20 @@ public class FilmLoaderTest {
 	
 	@Test
 	public void tt() throws Exception{
-		
+//		Date dd = new Date();
+//		Date dd1 = new Date(1482219514450L);
+//		
+//		System.out.println(dd.toLocaleString()+"--"+dd1.toLocaleString());
+//		System.out.println(dd.getTime());
+//		Calendar calen = Calendar.getInstance();
+//		calen.add(Calendar.MINUTE, 30);
+//		System.out.println(calen.toString());
+//		Date date = new Date(1482222131750L);
+//		System.out.println(date.toLocaleString());
+		Calendar calen = Calendar.getInstance(Locale.CHINA);
+		calen.set(2016, 11, 20,28,0,0);
+		calen.add(Calendar.DATE, 1);
+//		calen.add(Calendar.MINUTE, 107);
 	} 
 
 }
