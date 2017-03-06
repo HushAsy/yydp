4d936cfb2e4e85cca6553719a2adb26dea4c6cac
何海水 <270232645@qq.com>
2017-03-06 15:06:24 +0800

    测试1

---
+++ yydp/src/main/java/org/lf/yydp/action/TestController.java
@@ -0,0 +1,14 @@
+package org.lf.yydp.action;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+@Controller
+public class TestController {
+    public int testMethod(){
+        int i = 0;
+        return 0;
+    }
+}
